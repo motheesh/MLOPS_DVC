c700c7151a86806495d24898ea83296de205fd5f
Motheeshkumar J <motheesh96@gmail.com>
2021-09-16 23:28:01 +0530

    adding get_data.py

---
+++ src/get_data.py
@@ -0,0 +1,23 @@
+import os
+import yaml
+import argparse
+import pandas as pd
+
+def get_params(config_path):
+    with open(config_path) as yaml_file:
+        config=yaml.safe_load(yaml_file)
+        return config
+
+def get_data(config_path):
+    params=get_params(config_path)
+    source_data_path=params["data_source"]["source_path"]
+    seperator=params["data_source"]["seperator"]
+    data=pd.read_csv(source_data_path,sep=seperator)
+    return data
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config",default="params.yaml")
+    parsed_args=args.parse_args()
+    get_data(config_path=parsed_args.config)
